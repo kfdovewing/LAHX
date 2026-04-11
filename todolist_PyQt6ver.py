e827426227b7271dad58bea2eeb77542433d944f
Olivia Oh <olivia@Olivias-MacBook-Air.local>
2026-04-11 16:11:07 -0700

    main

--- todolist_PyQt6ver.py
+++ todolist_PyQt6ver.py
@@ -170,7 +170,6 @@ class TodoList(QWidget):
             self.tasks.clear()
             self.refresh_list()
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = TodoList()
