7ae60e9b689494900dcc0431bd1627aaccb4b05d
MoySquared <moyin7000@gmail.com>
2026-04-11 22:03:53 -0700

    shop

--- shop.py
+++ shop.py
@@ -1,15 +1,35 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton, QMainWindow
 from PyQt6.QtCore import QTimer
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 
 app = QApplication(sys.argv)
 
 window = QWidget()
 window.setWindowTitle("Shop")
-window.setGeometry(100, 100, 400, 300)
+window.setGeometry(100, 100, 500, 300)
 
 grid = QGridLayout()
 window.setLayout(grid)
+
+
+# shop background
+bg = QLabel(window)
+bg_pix = QPixmap("shop.png")
+
+bg_pix = bg_pix.scaled(
+    window.size(),
+    Qt.AspectRatioMode.KeepAspectRatio,
+    Qt.TransformationMode.SmoothTransformation
+)
+
+bg.setPixmap(bg_pix)
+bg.setGeometry(0, 0, 400, 300)
+bg.lower()
+
+window.showMaximized()
+
 
 money = 20
 food = 0
